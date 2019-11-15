8b4dd0fa4addd0cf392ddd43d1e6c599edd6faad
Artem Smirnov <a.smirnov@labster.pro>
2019-11-16 01:01:33 +0300

    Remove parcelable

--- app/src/main/java/com/g2pdev/simpletranslator/translation/model/TranslationModel.kt
+++ app/src/main/java/com/g2pdev/simpletranslator/translation/model/TranslationModel.kt
@@ -1,15 +1,14 @@
 package com.g2pdev.simpletranslator.translation.model
 
-import android.os.Parcelable
 import com.g2pdev.simpletranslator.translation.language.Language
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class TranslationModel(
     val language: Language,
     val name: String,
     var ordering: Int = 0
-) : Comparable<TranslationModel>, Parcelable {
+) : Comparable<TranslationModel> {
+
+    val isDeletable by lazy { language == Language.EN }
 
     override fun compareTo(other: TranslationModel): Int {
         return when {
