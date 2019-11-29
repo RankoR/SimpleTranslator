6d1a2fe81ae31ac5ed4e992196d1dbed781dc41f
Artem Smirnov <a.smirnov@labster.pro>
2019-11-29 22:15:14 +0300

    Move translation to separate module

--- translation/src/main/java/com/g2pdev/translation/translation/exception/ModelNotDownloadedException.kt
+++
@@ -1,7 +0,0 @@
-package com.g2pdev.translation.translation.exception
-
-import com.g2pdev.translation.translation.language.LanguagePair
-
-class ModelNotDownloadedException(
-    val languagePair: LanguagePair
-) : Exception()
--- translation/src/main/java/com/g2pdev/translation/translation/translator/Translator.kt
+++
@@ -1,8 +0,0 @@
-package com.g2pdev.translation.translation.translator
-
-import com.g2pdev.translation.translation.language.LanguagePair
-import io.reactivex.Single
-
-interface Translator {
-    fun translate(languagePair: LanguagePair, text: String): Single<String>
-}
